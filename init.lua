b02e80ab317fd1e9bf34dbeddd1b26f5d640a6ac
orip <oriori1703@gmail.com>
2026-07-25 20:54:30 +0300

    Update section 2's name

--- init.lua
+++ init.lua
@@ -195,7 +195,7 @@ do
 end
 
 -- ============================================================
--- SECTION 2: KEYMAPS
+-- SECTION 2: KEYMAPS & AUTOCMDS
 -- basic keymaps, basic autocmds
 -- ============================================================
 do
