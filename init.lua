19744209a8cc232e3b492750e5d8ce16bd7b581b
nicbenji <172269214+nicbenji@users.noreply.github.com>
2026-08-20 17:34:32 +0200

    less intrusive autocmp

--- init.lua
+++ init.lua
@@ -1014,6 +1014,15 @@ do
       -- See `:help blink-cmp-config-keymap` for defining your own keymap
       preset = 'default',
 
+      ['<C-Space>'] = {
+        function(cmp)
+          if cmp.is_visible() then
+            return cmp.hide()
+          else
+            return cmp.show()
+          end
+        end,
+      },
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
     },
@@ -1025,9 +1034,14 @@ do
     },
 
     completion = {
+      menu = { auto_show = false },
+      trigger = {
+        show_on_keyword = false,
+        show_on_trigger_character = false,
+      },
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
-      documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
     },
 
     sources = {
